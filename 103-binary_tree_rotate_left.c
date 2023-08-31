994f4dbf4609fb8a92e7187ffd0e47242fa04a78
niyioo <niyiolagbegi@gmail.com>
2023-08-31 07:03:30 -0700

    103

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,18 +8,18 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new_root;
+	binary_tree_t *new_root;
 
-    if (!tree || !tree->right)
-	    return (tree);
+	if (!tree || !tree->right)
+		return (tree);
 
-    new_root = tree->right;
-    tree->right = new_root->left;
-    if (new_root->left)
-    new_root->left->parent = tree;
-    new_root->left = tree;
-    new_root->parent = tree->parent;
-    tree->parent = new_root;
+	new_root = tree->right;
+	tree->right = new_root->left;
+	if (new_root->left)
+		new_root->left->parent = tree;
+	new_root->left = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
 
-    return (new_root);
+	return (new_root);
 }
